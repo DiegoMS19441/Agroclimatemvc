c86bfd7c633913baa89a4050e12fd4ae9f62dc20
Diego.M <127765045+DiegoMS19441@users.noreply.github.com>
2024-10-13 21:29:37 -0300

    Update Agricultor.java

--- src/main/java/br/com/fiap/agroclimate/agroclimatemvc/model/Agricultor.java
+++ src/main/java/br/com/fiap/agroclimate/agroclimatemvc/model/Agricultor.java
@@ -1,11 +1,17 @@
 package br.com.fiap.agroclimate.agroclimatemvc.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.validator.constraints.br.CPF;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Getter@Setter
@@ -20,12 +26,25 @@ public class Agricultor {
     private Long id;
 
     @Column(name = "NM_AGRIGULTOR", nullable = false)
+    @Size(min = 3, max = 90)
+    @NotBlank(message = "Nome é um campo obrigatório")
     private String nome;
 
+    @Column(name = "CPF_AGRICULTOR",nullable = false)
+    @CPF(message = "Verifique os seus dados")
+    private String cpf;
+
+    @Column(name = "DT_NASCIMENTO")
+    @Past(message = "Verifique sua data de nascimento")
+    private LocalDate nascimento;
+
     @Column(name = "EMAIL_AGRICULTOR", unique = true, nullable = false)
+    @Email(message = "Verifique seu e-mail")
+    @NotBlank(message = "E-mail é um campo obrigatório")
     private String email;
 
     @Column(name = "SENHA_AGRICULTOR", nullable = false)
+    @NotBlank(message = "Senha é um campo obrigatório")
     private String senha;
 
     @OneToMany(mappedBy = "agricultor", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.EAGER)
